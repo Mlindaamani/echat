72aa889fc56dba35b542944414de6bbcb508c2bb
mlindaamani <mlindaadolf@gmail.com>
2025-02-28 16:54:36 +0300

    Updated the environment variables

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
+const MONGO_URI =
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGO_URI_DEV
+    : process.env.MONGO_URI_PROD;
+
+console.log(MONGO_URI);
+
 const connnectToMongoDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_DEV);
+    const conn = await mongoose.connect(MONGO_URI);
     console.log(
       `✔️  Success! Mongodb is running on port: ${conn.connection.port}`
     );
