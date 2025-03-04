59640183ca788771578b6ac2fe4dd89d9e4f47b2
mlindaamani <mlindaadolf@gmail.com>
2025-03-04 12:15:10 +0300

    Removed redundant settings

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -13,21 +13,6 @@ export const Sidebar = () => (
       <SidebarLink to="/earnings" icon={balance} text="Earnings" />
       <SidebarLink to="/room" icon={person} text="Room" />
       <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
-      <SidebarLink to="/settings" icon={settings} text="Settings" />
     </div>
   </div>
 );
